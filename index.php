81a1761c2672418ad2e61ca0dfa6aee06da084d7
Swann <swann.herrera@gmail.com>
2019-11-24 15:39:37 +0100

    commit body style fenêtre

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <title>Cours WebGL</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <style>
+    body {
+        padding: 0;
+        margin : 0;
+    }
+    </style>
 </head>
 <body>
 <script type="module">
