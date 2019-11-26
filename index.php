008de28e48b54c67cb74d52cab90f772c6aa3caa
noe <noelarrieulacoste@yahoo.Fr>
2019-11-26 19:48:09 +0100

    create function

--- index.php
+++ index.php
@@ -30,33 +30,27 @@
 
         camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 3000);
         camera.position.z = 300;
-        camera.position.y = 230;
+        camera.position.y = 180;
         //camera.position.x = -100;
-        camera.rotation.x -= 0.4;
+        camera.rotation.x -= 0.5;
         var geometry;
 
         /**
          * Textures matériel
          */
-
-        var textMur = new THREE.TextureLoader().load('./tex/stone wall 7.png');
-        var textLMur = new THREE.TextureLoader().load('./tex/stone wall 7.png');
-        var textRempart = new THREE.TextureLoader().load('./tex/stone wall 7.png');
+        var textMurPath = './tex/stone wall 7.png';
+        var textRoofPath = './tex/roof.jpg';
+        var textWindowPath = './tex/window.jpg';
         var textGrass = new THREE.TextureLoader().load('./tex/grass1.png');
         var textPorte = new THREE.TextureLoader().load('./tex/door.jpg');
-        var textGate = new THREE.TextureLoader().load('./tex/stone wall 7.png');
+        var textGate = new THREE.TextureLoader().load(textMurPath);
         var textToit = new THREE.TextureLoader().load('./tex/roof.jpg');
-        var textWindow = new THREE.TextureLoader().load('./tex/window.jpg');
         var textSky = new THREE.TextureLoader().load('./tex/sky.jpg');
 
-        var materialLMur = new THREE.MeshBasicMaterial({map: textLMur});
-        var materialMur = new THREE.MeshBasicMaterial({map: textMur});
-        var materialRempart = new THREE.MeshBasicMaterial({map: textRempart});
         var materialGrass = new THREE.MeshBasicMaterial({map: textGrass});
         var materialPorte = new THREE.MeshBasicMaterial({map: textPorte});
         var materialGate = new THREE.MeshBasicMaterial({map: textGate});
         var materialToit = new THREE.MeshBasicMaterial({map: textToit});
-        var materialWindow = new THREE.MeshBasicMaterial({map: textWindow});
 
         /**
          * Taille des objects
@@ -80,7 +74,7 @@
         var MurailleDepth = 20;
         //remparts
         var RempartWidth = 8;
-        var RempartHeight = 5;
+        var RempartHeight = 10;
         var RempartDepth = 2;
         //tour
         var TowerRadius = 20;
@@ -88,14 +82,14 @@
         var TowerRadialSegment = 32;
         var TowerRoofRadiusTop = 0;
         var TowerRoofRadiusBottom = 25;
-        var TowerRoofHeight = 20;
+        var TowerRoofHeight = 30;
         var TowerRoofRadialSegment = 32;
         var TowerWindowWidth = 10;
         var TowerWindowHeigth = 10;
         var TowerWindowDepth = 3;
         //donjon
         var DonjonRadius = 40;
-        var DonjonHeight = 100;
+        var DonjonHeight = 130;
         var DonjonRadialSegment = 32;
         var DonjonRoofRadiusTop = 0;
         var DonjonRoofRadiusBottom = 50;
@@ -106,30 +100,20 @@
          * Répétition des textures
          */
 
-        textRempart.wrapS = THREE.RepeatWrapping;
-        textRempart.wrapT = THREE.RepeatWrapping;
-        textRempart.repeat.set(RempartWidth / 20, RempartHeight / 20);
-        textLMur.wrapS = THREE.RepeatWrapping;
-        textLMur.wrapT = THREE.RepeatWrapping;
-        textLMur.repeat.set(MurailleWidth / 20, MurailleHeight / 20);
-        textMur.wrapS = THREE.RepeatWrapping;
-        textMur.wrapT = THREE.RepeatWrapping;
-        textMur.repeat.set(FrontMurailleWidth / 20, MurailleHeight / 20);
+
         textGate.wrapS = THREE.RepeatWrapping;
         textGate.wrapT = THREE.RepeatWrapping;
-        textGate.repeat.set(GateWidth/20, GateHeight/20);
-        textToit.wrapS = THREE.RepeatWrapping;
-        textToit.wrapT = THREE.RepeatWrapping;
-        textToit.repeat.set(2, 1);
+        textGate.repeat.set(GateWidth / 20, GateHeight / 20);
+
         textGrass.wrapS = THREE.RepeatWrapping;
         textGrass.wrapT = THREE.RepeatWrapping;
-        textGrass.repeat.set(100,100);
+        textGrass.repeat.set(100, 100);
 
 
         /**
          * Ajout du terrain
          */
-        geometry = new THREE.CircleGeometry(1000,64);
+        geometry = new THREE.CircleGeometry(1000, 64);
         terrain = new THREE.Mesh(geometry, materialGrass);
         terrain.rotateX(Math.radians(-90));
         scene.add(terrain);
@@ -138,40 +122,25 @@
         /**
          * Muraille avant
          */
-        var frontMuraille = new THREE.Group();
-
-        geometry = new THREE.CubeGeometry(FrontMurailleWidth, MurailleHeight, MurailleDepth);
-        mesh = new THREE.Mesh(geometry, materialMur);
-        mesh.position.y = MurailleHeight / 2;
-        frontMuraille.add(mesh.clone());
-
-        for (let initX = -(FrontMurailleWidth / 2) + (RempartWidth / 2); initX <= FrontMurailleWidth / 2; initX += 20) {
-            geometry = new THREE.CubeGeometry(RempartWidth, RempartHeight, RempartDepth);
-            mesh = new THREE.Mesh(geometry, materialRempart);
-            mesh.position.set(initX, MurailleHeight + RempartHeight / 2, (MurailleDepth - RempartDepth) / 2);
-            frontMuraille.add(mesh.clone());
-            mesh.position.z = (-MurailleDepth + RempartDepth) / 2;
-            frontMuraille.add(mesh.clone());
-        }
-
+        var frontMuraille = createMuraille(FrontMurailleWidth, MurailleHeight, MurailleDepth,
+            RempartWidth, RempartHeight, RempartDepth, textMurPath);
         /**
          * Muraille longue
          */
-        var muraille = new THREE.Group();
-        geometry = new THREE.CubeGeometry(MurailleWidth, MurailleHeight, MurailleDepth);
-        mesh = new THREE.Mesh(geometry, materialLMur);
-        mesh.position.y = MurailleHeight / 2;
-        muraille.add(mesh.clone());
-
-        for (let initX = -(MurailleWidth / 2) + (RempartWidth / 2); initX <= MurailleWidth / 2; initX += 20) {
-            geometry = new THREE.CubeGeometry(RempartWidth, RempartHeight, RempartDepth);
-            mesh = new THREE.Mesh(geometry, materialRempart);
-            mesh.position.set(initX, MurailleHeight + RempartHeight / 2, (MurailleDepth - RempartDepth) / 2);
-            muraille.add(mesh.clone());
-            mesh.position.z = (-MurailleDepth + RempartDepth) / 2;
-            muraille.add(mesh.clone());
-        }
-
+        var muraille = createMuraille(MurailleWidth, MurailleHeight, MurailleDepth,
+            RempartWidth, RempartHeight, RempartDepth, textMurPath);
+        /**
+         * Tour
+         */
+        var tour = createTower(TowerRadius, TowerHeight, TowerRadialSegment, textMurPath,
+            TowerRoofRadiusTop, TowerRoofRadiusBottom, TowerRoofHeight, TowerRoofRadialSegment, textRoofPath,
+            TowerWindowWidth, TowerWindowHeigth, TowerWindowDepth, textWindowPath);
+        /**
+         * Donjon
+         */
+        var donjon = createTower(DonjonRadius,DonjonHeight,DonjonRadialSegment,textMurPath,
+            DonjonRoofRadiusTop,DonjonRoofRadiusBottom,DonjonRoofHeight,DonjonRoofRadialSegment,textRoofPath,
+            TowerWindowWidth*2, TowerWindowHeigth*2, TowerWindowDepth, textWindowPath);
         /**
          * Entrée + porte
          */
@@ -194,50 +163,6 @@
         entree.add(mesh.clone());
         entree.add(mesh2.clone());
         entree.add(mesh3.clone());
-        /**
-         * Tour
-         */
-
-        var tour = new THREE.Group();
-        mesh = new THREE.Mesh(new THREE.CylinderBufferGeometry(TowerRadius, TowerRadius, TowerHeight,
-            TowerRadialSegment), materialMur);
-        mesh.position.y = TowerHeight / 2;
-
-        mesh2 = new THREE.Mesh(new THREE.CylinderBufferGeometry(TowerRoofRadiusTop, TowerRoofRadiusBottom,
-            TowerRoofHeight, TowerRoofRadialSegment), materialToit);
-        mesh2.position.y = TowerHeight + TowerRoofHeight / 2;
-
-        mesh3 = new THREE.Mesh(new THREE.BoxGeometry(TowerWindowWidth, TowerWindowHeigth, TowerWindowDepth),
-            materialWindow);
-        mesh3.position.set(0, TowerHeight - TowerWindowHeigth / 2 - 5, TowerRadius - TowerWindowDepth / 2);
-        tour.add(mesh3.clone());
-        mesh3.position.set(0, TowerHeight - TowerWindowHeigth / 2 - 5, -TowerRadius + TowerWindowDepth / 2);
-        tour.add(mesh3.clone());
-        mesh3.rotateY(Math.radians(90));
-        mesh3.position.set(TowerRadius - TowerWindowDepth / 2, TowerHeight - TowerWindowHeigth / 2 - 5, 0);
-        tour.add(mesh3.clone());
-        mesh3.position.set(-TowerRadius + TowerWindowDepth / 2, TowerHeight - TowerWindowHeigth / 2 - 5, 0);
-        tour.add(mesh3.clone());
-
-        tour.add(mesh.clone());
-        tour.add(mesh2.clone());
-
-        /**
-         * Donjon
-         */
-
-        var donjon = new THREE.Group();
-        mesh = new THREE.Mesh(new THREE.CylinderBufferGeometry(DonjonRadius, DonjonRadius, DonjonHeight,
-            DonjonRadialSegment), materialMur);
-        mesh.position.y = DonjonHeight / 2;
-
-        mesh2 = new THREE.Mesh(new THREE.CylinderBufferGeometry(DonjonRoofRadiusTop, DonjonRoofRadiusBottom,
-            DonjonRoofHeight, DonjonRoofRadialSegment), materialToit);
-        mesh2.position.y = DonjonHeight + DonjonRoofHeight / 2;
-        donjon.add(mesh.clone());
-        donjon.add(mesh2.clone());
-
-
         /**
          * Affichage
          */
@@ -275,7 +200,7 @@
          * Options de rendu
          */
         scene.background = textSky;
-        scene.fog = new THREE.Fog(0x6086b0, 600,1000);
+        scene.fog = new THREE.Fog(0x6086b0, 600, 1000);
         renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
@@ -288,6 +213,88 @@
             group.add(mesh.clone());
         }
         scene.add(group);
+    }
+
+    function createMuraille(MurailleWidth, MurailleHeight, MurailleDepth,
+                            RempartWidth, RempartHeight, RempartDepth, textPath) {
+        var geometry;
+        var textRempart = new THREE.TextureLoader().load(textPath);
+        var textMur = new THREE.TextureLoader().load(textPath);
+        textRempart.wrapS = THREE.RepeatWrapping;
+        textRempart.wrapT = THREE.RepeatWrapping;
+        textRempart.repeat.set(RempartWidth / 20, RempartHeight / 20);
+        textMur.wrapS = THREE.RepeatWrapping;
+        textMur.wrapT = THREE.RepeatWrapping;
+        textMur.repeat.set(MurailleWidth / 20, MurailleHeight / 20);
+        var materialMur = new THREE.MeshBasicMaterial({map: textMur});
+        var materialRempart = new THREE.MeshBasicMaterial({map: textRempart});
+
+
+        var Muraille = new THREE.Group();
+
+
+        geometry = new THREE.CubeGeometry(MurailleWidth, MurailleHeight, MurailleDepth);
+        mesh = new THREE.Mesh(geometry, materialMur);
+        mesh.position.y = MurailleHeight / 2;
+        Muraille.add(mesh.clone());
+
+        for (let initX = -(MurailleWidth / 2) + (RempartWidth / 2); initX <= MurailleWidth / 2; initX += 20) {
+            geometry = new THREE.CubeGeometry(RempartWidth, RempartHeight, RempartDepth);
+            mesh = new THREE.Mesh(geometry, materialRempart);
+            mesh.position.set(initX, MurailleHeight + RempartHeight / 2, (MurailleDepth - RempartDepth) / 2);
+            Muraille.add(mesh.clone());
+            mesh.position.z = (-MurailleDepth + RempartDepth) / 2;
+            Muraille.add(mesh.clone());
+        }
+        return Muraille;
+    }
+
+    function createTower(TowerRadius, TowerHeight, TowerRadialSegment, textMurPath,
+                         TowerRoofRadiusTop, TowerRoofRadiusBottom, TowerRoofHeight, TowerRoofRadialSegment, textToitPath,
+                         TowerWindowWidth = null, TowerWindowHeigth, TowerWindowDepth, windowTextPath) {
+        var tour = new THREE.Group();
+        var textMur = new THREE.TextureLoader().load(textMurPath);
+        var textToit = new THREE.TextureLoader().load(textToitPath);
+        var materialMur = new THREE.MeshBasicMaterial({map: textMur});
+        var materialToit = new THREE.MeshBasicMaterial({map: textToit});
+        if (TowerWindowWidth !== null) {
+            var textWindow = new THREE.TextureLoader().load(windowTextPath);
+            var materialWindow = new THREE.MeshBasicMaterial({map: textWindow});
+        }
+        textMur.wrapS = THREE.RepeatWrapping;
+        textMur.wrapT = THREE.RepeatWrapping;
+        textMur.repeat.set(TowerRadius/5, TowerHeight / 20);
+        textToit.wrapS = THREE.RepeatWrapping;
+        textToit.wrapT = THREE.RepeatWrapping;
+        textToit.repeat.set(2, 1);
+
+
+        mesh = new THREE.Mesh(new THREE.CylinderBufferGeometry(TowerRadius, TowerRadius, TowerHeight,
+            TowerRadialSegment), materialMur);
+        mesh.position.y = TowerHeight / 2;
+
+        mesh2 = new THREE.Mesh(new THREE.CylinderBufferGeometry(TowerRoofRadiusTop, TowerRoofRadiusBottom,
+            TowerRoofHeight, TowerRoofRadialSegment), materialToit);
+        mesh2.position.y = TowerHeight + TowerRoofHeight / 2;
+
+        if (TowerWindowWidth !== null) {
+            mesh3 = new THREE.Mesh(new THREE.BoxGeometry(TowerWindowWidth, TowerWindowHeigth, TowerWindowDepth),
+                materialWindow);
+            mesh3.position.set(0, TowerHeight - TowerWindowHeigth / 2 - 5, TowerRadius - TowerWindowDepth / 2);
+            tour.add(mesh3.clone());
+            mesh3.position.set(0, TowerHeight - TowerWindowHeigth / 2 - 5, -TowerRadius + TowerWindowDepth / 2);
+            tour.add(mesh3.clone());
+            mesh3.rotateY(Math.radians(90));
+            mesh3.position.set(TowerRadius - TowerWindowDepth / 2, TowerHeight - TowerWindowHeigth / 2 - 5, 0);
+            tour.add(mesh3.clone());
+            mesh3.position.set(-TowerRadius + TowerWindowDepth / 2, TowerHeight - TowerWindowHeigth / 2 - 5, 0);
+            tour.add(mesh3.clone());
+        }
+
+        tour.add(mesh.clone());
+        tour.add(mesh2.clone());
+
+        return tour;
     }
 
 
